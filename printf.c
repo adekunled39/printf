3072611f2038716a4f62da7f9b525bcdb266a1e9
adekunled39 <adekunled39@gmail.com>
2022-05-25 06:08:49 -0700

    task 0 helper

---
+++ printf.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* _printf - prints to stdout
+* @format: the char format to print
+* Return: the num of bytes written
+*/
+
+int _printf(const char *format, ...)
+
+{
+
+int count;
+va_list args;
+prnt_fmt prnt_f[] = {
+{"c", _putarg_char},
+{"s", _putstr},
+{"%", _putper},
+{"d", _putint},
+{"i", _putint},
+{NULL, NULL},
+};
+if (format == NULL)
+{
+
+return (-1);
+
+}
+va_start(args, format);
+count = 0;
+count = prnt_format(prnt_f, format, args);
+va_end(args);
+return (count);
+}
